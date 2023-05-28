1228d7b8b617360a2a7b08af98e92781b7f58ca0
Salman <59947811+elated-salman@users.noreply.github.com>
2023-05-28 20:48:36 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install python3-pip
-sudo git clone https://github.com/9573030999/Medical-Insurance.git
+sudo git clone https://github.com/elated-salman/Medical-Insurance.git
 cd Medical-Insurance
 sudo pip3 install -r requirements.txt
 sudo python3 app.py
