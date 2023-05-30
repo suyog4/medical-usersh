7710fcf6f7eac16587c59e08bf3dad0f37a6f92c
suyog4 <88851423+suyog4@users.noreply.github.com>
2023-05-30 15:26:07 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install python3-pip
-sudo git clone https://github.com/elated-salman/Medical-Insurance.git
+sudo git clone https://github.com/suyog4/Medical-Insurance.git
 cd Medical-Insurance
 sudo pip3 install -r requirements.txt
 sudo python3 app.py
